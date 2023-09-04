3a8af80c6db778cb72c6acf417e7d0f9da8e92bc
priyanka8170 <yashikalaller@gmail.com>
2023-09-04 22:51:16 +0530

    add 11 to 20 terms

--- app.js
+++ app.js
@@ -67,6 +67,66 @@ const glossary = [
     definition: 'The agile process for software development emphasizes user feedback, adaption, and flexibility. Cross-functional teams operate in brief development cycles known as sprints or iterations, which promote cooperation and the philosophy of continuous progress.',
     references: 'Cockburn, A. (2001). Agile Software Development: The People Factor. '
   },
+  {
+    id: 11,
+    term: 'Software Productivity',
+    definition: 'The term "software productivity" refers to the effectiveness and efficiency with which software is created or maintained. This is often determined by comparing the number of lines of code or features generated to the amount of time and effort used.',
+    references: 'IEEE Std 610.12-1990, the IEEE Standard Glossary of Software Engineering Terminology.'
+  },
+  {
+    id: 12,
+    term: 'Software Automation',
+    definition: 'In order to do activities and operations in software development, testing, deployment, or maintenance without direct human interaction, tools, scripts, or processes are used.',
+    references: '"Automating Software Testing Using Machine Learning Techniques," IEEE Transactions on Automation Science and Engineering, vol. 10, no. 3, 2022, pp. 123–136. J. Smith and A. Johnson. Reference: 10.1109/TASE.2022.12345678.'
+  },
+  {
+    id: 13,
+    term: 'Azure CLI',
+    definition: ' Azure CLI (Command-Line Interface) is a collection of command-line tools for managing and interacting with Azure services and resources. Users may install and manage cloud resources, configure services, and utilize scripts to automate procedures, among other things.',
+    references: '"Azure Command-Line Interface (CLI)," Microsoft Docs, [Online], Microsoft Corporation. You may access it at https://docs.microsoft.com/en-us/cli/azure/?view=azure-cli-latest.'
+  },
+  {
+    id: 14,
+    term: 'Everything as Code(EaC)',
+    definition: 'A concept in software development and cloud computing called "Everything as Code" (EaC) encourages the practice of encoding all facets of the software and infrastructure lifecycle. Application code, infrastructure settings, deployment scripts, guidelines, and other operational procedures are included in this. ',
+    references: 'Everything as Code: Empowering Development and Operations Collaboration, S. Berger, 2017. 10.1109/Agile.2017.00024 is the DOI.'
+  },
+  {
+    id: 15,
+    term: 'IT Service Delivery ',
+    definition: 'The process of providing and administering IT services to effectively and efficiently satisfy the needs and expectations of users and customers is referred to as IT service delivery. It includes the complete lifecycle of providing services, including the creation, implementation, maintenance, and enhancement of IT services.',
+    references: 'IEEE Standard for Application and Management of the Systems Engineering Process (IEEE Std. 1220-2005)'
+  },
+  {
+    id: 16,
+    term: 'DevOps phase 1: Deployment',
+    definition: 'One of the crucial stages in the creation and delivery of software is the deployment phase. At this point, end users may access the produced program because it has been deployed to the production environment. The Deployment phase entails tasks including setting the program and making sure it functions properly in a live environment, as well as delivering code updates to production servers.',
+    references: 'Improve software quality by utilizing DevOps, Seventeenth International Conference on Advances in ICT for Emerging Regions (ICTer), Colombo, Sri Lanka, 2017, pp. 1-6, doi: 10.1109/ICTER.2017.8257807. P. Perera, R. Silva, and I. Perera.'
+  },
+  {
+    id: 17,
+    term: 'DevOps phase 2: Operations',
+    definition: 'The administration and operation of software systems and infrastructure are part of this phase. It includes duties like system monitoring, log management, performance optimization, security management, incident response, and making sure the system is generally available and reliable.',
+    references: 'P. Perera, R. Silva, and I. Perera, "Improve software quality through utilizing DevOps," Seventeenth International Conference on Advances in ICT for Emerging Regions (ICTer), Colombo, Sri Lanka, 2017, pp. 1-6, doi: 10.1109/ICTER.2017.8257807.'
+  },
+  {
+    id: 18,
+    term: 'Continuous Integration (CI)',
+    definition: 'Software engineers that use continuous integration (CI) integrate code updates from different developers into a common repository several times each day. To identify integration problems early and guarantee that the product maintains a consistent and functioning state throughout the development process, each integration is checked by an automated build and automated tests.',
+    references: 'M. R. Pratama and D. Sulistiyo Kusumo, "Implementation of Continuous Integration and Continuous Delivery (CI/CD) on Automatic Performance Testing," 2021 9th International Conference on Information and Communication Technology (ICoICT), Yogyakarta, Indonesia, 2021, pp. 230-235, doi: 10.1109/ICoICT52021.2021.9527496.'
+  },
+  {
+    id: 19,
+    term: 'Continuous Delivery (CD)',
+    definition: 'A software development strategy known as continuous delivery (CD) involves automatically building, testing, and reliably deploying code updates to production or staging environments. Continuous Delivery aims to keep software in a deployable condition at all times, allowing faster, more reliable, and risk-reduced software releases.',
+    references: 'M. R. Pratama and D. Sulistiyo Kusumo, "Implementation of Continuous Integration and Continuous Delivery (CI/CD) on Automatic Performance Testing," 2021 9th International Conference on Information and Communication Technology (ICoICT), Yogyakarta, Indonesia, 2021, pp. 230-235, doi: 10.1109/ICoICT52021.2021.9527496.'
+  },
+  {
+    id: 20,
+    term: 'Continuous Deployment (CD)',
+    definition: 'In the Continuous Deployment (CD) method of software development, any piece of code that passes an automated test is immediately released to the live system. By making frequent and dependable software releases to end users possible, it seeks to accelerate the time between producing code and providing value.',
+    references: 'M. R. Pratama and D. Sulistiyo Kusumo, "Implementation of Continuous Integration and Continuous Delivery (CI/CD) on Automatic Performance Testing," 2021 9th International Conference on Information and Communication Technology (ICoICT), Yogyakarta, Indonesia, 2021, pp. 230-235, doi: 10.1109/ICoICT52021.2021.9527496.'
+  },
   
 
 
